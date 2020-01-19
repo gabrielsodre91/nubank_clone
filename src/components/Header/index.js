252b9bd32a25a371d7a1dcbece8147f672bd3a9e
Gabriel <gabrielsodre91@gmail.com>
2020-01-19 20:52:36 -0300

    Iniciando

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+import { Container } from './styles';
+
+export default function Header() {
+    return (
+        <Container>
+            {/* <Top>
+                <Logo source={} />
+                <Title>Gabriel</Title>
+            </Top> */}
+        </Container>
+    )
+}
